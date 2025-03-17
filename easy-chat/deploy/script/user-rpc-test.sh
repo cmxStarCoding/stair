89e62f8c9a4efd05af6e21b01b808ae203d2af49
cmx_star <316785038@qq.com>
2025-03-17 22:02:41 +0800

    更改

--- easy-chat/deploy/script/user-rpc-test.sh
+++ easy-chat/deploy/script/user-rpc-test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-reso_addr='registry.cn-hangzhou.aliyuncs.com/easy-chat/user-rpc-test'
+reso_addr='crpi-5vfnm5k3tdyjsxrh.cn-hangzhou.personal.cr.aliyuncs.com/cmx-easy-chat/user-rpc-dev'
 tag='latest'
 
 container_name="easy-chat-user-rpc-test"
@@ -15,4 +15,4 @@ docker pull ${reso_addr}:${tag}
 
 # 如果需要指定配置文件的
 # docker run -p 10001:8080 --network imooc_easy-chat -v /easy-chat/config/user-rpc:/user/conf/ --name=${container_name} -d ${reso_addr}:${tag}
-docker run -p 10001:8080  --name=${container_name} -d ${reso_addr}:${tag}
+docker run -p 10000:10000  --name=${container_name} -d ${reso_addr}:${tag}
