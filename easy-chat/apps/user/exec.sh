331c34160f551458c02299885a3f69d568347dac
cmx_star <316785038@qq.com>
2025-03-23 23:25:01 +0800

    登录注册功能

--- easy-chat/apps/user/exec.sh
+++ easy-chat/apps/user/exec.sh
@@ -2,3 +2,6 @@
 goctl rpc protoc ./apps/user/rpc/user.proto --go_out=./apps/user/rpc/ --go-grpc_out=./apps/user/rpc/ --zrpc_out=./apps/user/rpc/
 #生成mysql
 goctl model mysql ddl -src="./deploy/sql/user.sql" -dir="./apps/user/models/" -c
+#用户api
+goctl api go -api apps/user/api/user.api -dir apps/user/api -style gozero
+
